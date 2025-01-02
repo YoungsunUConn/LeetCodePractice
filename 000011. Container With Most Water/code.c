cfe27e757bc9fa5d7c4f85bbec2b9c0480ff5979
YoungsunUConn <114104493+YoungsunUConn@users.noreply.github.com>
2025-01-02 16:39:49 -0500

    Update code.c

--- 000011. Container With Most Water/code.c
+++ 000011. Container With Most Water/code.c
@@ -1,20 +1,31 @@
 int maxArea(int* height, int heightSize) {
     // find points x and y
     // water_amount = (loc_x - loc_y) * min(x,y)
-    // O(n^2) solution
 
     int i, j;
     int max_output = -1;
+    int max_height = -1;
+    
 
     for(i = 0 ; i < heightSize; i ++) {
         int max_len = heightSize - i - 1;
+        int possible_max = height[i] * max_len;
 
         // this height has a potential to proceed
-        if(height[i] * max_len > max_output ) {
+        if(possible_max > max_output ) {
             int output;
-            for(j = i+1 ; j < heightSize ; j ++) {
+            for(j = heightSize - 1 ; j > i ; j --) {
+                
                 output = (j - i) * (height[i] < height[j] ? height[i] : height[j]);
-                if( output > max_output) max_output = output;
+
+                if( output > max_output) {
+                    max_output = output;
+
+                    // no reason to search further
+                    if(height[j] >= height[i]) {
+                        break;
+                    }
+                }
             }
         }
     }
